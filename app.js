5360ce51f99a2fd4d619c8981ab68cea02b17221
Harshit Bhardwaj <harshitbhardwaj97@gmail.com>
2024-02-18 00:14:47 +0530

    Exercise 11.12 /health endpoint broken version Added

--- app.js
+++ app.js
@@ -11,9 +11,11 @@ app.listen(PORT, () => {
 });
 
 app.get("/version", (req, res) => {
-  res.send("3");
+  res.send("4"); // App should show 3 only, since this time deployment will fail
 });
 
 app.get("/health", (req, res) => {
+  // eslint-disable-next-line no-constant-condition
+  if (true) throw "error...  ";
   res.send("ok");
 });
